b37538acc0b65260e8255d944890fd8b3d00d04c
Jade Huynh <35jadeh@gmail.com>
2019-07-02 16:28:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,11 +18,21 @@ function divide()
 
 function inc(n)
 {
-  
+  return n++
 }
 
 function dec(n)
 {
-  
+  return n--
+}
+
+function makeInt(n)
+{
+  return parseInt(n, 10)
+}
+
+function preserveDecimal(n)
+{
+  return parseFloat(n)
 }
 
